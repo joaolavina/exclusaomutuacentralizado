48eebb52aab1ba1db0c441502bbf73057b21829b
Gabriel <grcestari@gmail.com>
2025-08-20 20:06:38 -0300

    começando processo

--- src/main/java/com/trabalho/Processo.java
+++ src/main/java/com/trabalho/Processo.java
@@ -1,5 +1,36 @@
 package com.trabalho;
 
+import java.util.Random;
+import java.util.UUID;
+
 public class Processo {
 
+    private String id;
+
+    public Processo(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String gerarId() {
+        String uniqueId = UUID.randomUUID().toString();
+        return uniqueId;
+    }
+
+    public void tempoComsumo(int id) {
+
+        try{
+            int tempoAcesso = new Random().nextInt(16) + 10;
+            Thread.sleep(tempoAcesso * 1000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 }
